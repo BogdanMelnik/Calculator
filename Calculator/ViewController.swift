749172c96da8e1af1484a2afef0e223963b8235e
Bogdan <melnik.bogdan@icloud.com>
2016-08-08 17:50:09 +0300

    Result label resizing added successfully.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -6,20 +6,271 @@
 //  Copyright © 2016 Yohoho. All rights reserved.
 //
 
+extension UIButton {
+    func setBackgroundColor(color: UIColor, forState: UIControlState) {
+        UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
+        CGContextSetFillColorWithColor(UIGraphicsGetCurrentContext(), color.CGColor)
+        CGContextFillRect(UIGraphicsGetCurrentContext(), CGRect(x: 0, y: 0, width: 1, height: 1))
+        let colorImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        self.setBackgroundImage(colorImage, forState: forState)
+}}
+
 import UIKit
 
-class ViewController: UIViewController {
+class LabelWithAdaptiveTextHeight: UILabel {
+    
+   }
 
+class ViewController: UIViewController{
+    
+    //-= UI =-
+    @IBOutlet var resultLabel: StyledLabel!
+    
+    @IBOutlet var clearButton: UIButton!
+    @IBOutlet var changeSignButton: UIButton!
+    @IBOutlet var procentButton: UIButton!
+    @IBOutlet var divisionButton: UIButton!
+    @IBOutlet var multiplicationButton: UIButton!
+    @IBOutlet var subtractionButton: UIButton!
+    @IBOutlet var additionButton: UIButton!
+    @IBOutlet var equalsButton: UIButton!
+    @IBOutlet var decimalButton: UIButton!
+    
+    @IBOutlet var button7: UIButton!
+    @IBOutlet var button8: UIButton!
+    @IBOutlet var button9: UIButton!
+    @IBOutlet var button4: UIButton!
+    @IBOutlet var button5: UIButton!
+    @IBOutlet var button6: UIButton!
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    @IBOutlet var button0: UIButton!
+    
+    @IBOutlet var allButtons: [UIButton]!
+    
+    @IBAction func button7Pressed(sender: UIButton) {
+        operateInput("7")
+    }
+    @IBAction func button8Pressed(sender: UIButton) {
+        operateInput("8")
+    }
+    @IBAction func button9Pressed(sender: UIButton) {
+        operateInput("9")
+    }
+    @IBAction func button4Pressed(sender: UIButton) {
+        operateInput("4")
+    }
+    @IBAction func button5Pressed(sender: UIButton) {
+        operateInput("5")
+    }
+    @IBAction func button6Pressed(sender: UIButton) {
+        operateInput("6")
+    }
+    @IBAction func button1Pressed(sender: UIButton) {
+        operateInput("1")
+    }
+    @IBAction func button2Pressed(sender: UIButton) {
+        operateInput("2")
+    }
+    @IBAction func button3Pressed(sender: UIButton) {
+        operateInput("3")
+    }
+    @IBAction func button0Pressed(sender: UIButton) {
+        operateInput("0")
+    }
+    
+    @IBAction func clearButtonPressed(sender: UIButton) {
+        userInput = ""
+        accumulator = 0
+        updateDisplay()
+        numberStack.removeAll()
+        operatorStack.removeAll()
+    }
+    
+    @IBAction func changeSignButtonPressed(sender: UIButton) {
+        if userInput.isEmpty {
+            userInput = resultLabel.text!
+        }
+        operateInput("-")
+    }
+    
+    @IBAction func procentButtonPressed(sender: UIButton) {
+        //!!!
+    }
+    
+    @IBAction func divisionButtonPressed(sender: UIButton) {
+        doMath("/")
+    }
+    
+    @IBAction func multiplicationButtonPressed(sender: UIButton) {
+        doMath("*")
+    }
+    
+    @IBAction func subtractionButtonPressed(sender: UIButton) {
+        doMath("-")
+    }
+    
+    @IBAction func additionButtonPressed(sender: UIButton) {
+        doMath("+")
+    }
+    
+    @IBAction func equalsButtonPressed(sender: UIButton) {
+        doEquals()
+    }
+    
+    @IBAction func decimalButtonPressed(sender: UIButton) {
+        if hasIndex(stringToSearch: userInput, characterToFind: ".") == false {
+            operateInput(".")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.deviceRotated), name: UIDeviceOrientationDidChangeNotification, object: nil)
+        
+        // KVO ResultLabel signing
+        resultLabel.addObserver(self, forKeyPath: "text", options: NSKeyValueObservingOptions.New, context: nil)
+        
+        updateButtons()
+        resultLabel.text = "0"
+    }
+    
+    // KVO
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        if (keyPath == "text") {
+            resultLabel.layoutSubviews()
+        }
+    }
+    
+    func updateButtons() {
+        for button in allButtons {
+            let borderWidth:CGFloat = 2.0;
+            button.frame = CGRectInset(button.frame, -borderWidth, -borderWidth)
+            button.layer.borderColor = UIColor.whiteColor().CGColor
+            button.layer.borderWidth = borderWidth
+            button.setBackgroundColor(UIColor.darkGrayColor(), forState: UIControlState.Highlighted)
+        }
+        divisionButton.setBackgroundColor(UIColor.blackColor(), forState: UIControlState.Highlighted)
+        multiplicationButton.setBackgroundColor(UIColor.blackColor(), forState: UIControlState.Highlighted)
+        subtractionButton.setBackgroundColor(UIColor.blackColor(), forState: UIControlState.Highlighted)
+        additionButton.setBackgroundColor(UIColor.blackColor(), forState: UIControlState.Highlighted)
+        equalsButton.setBackgroundColor(UIColor.blackColor(), forState: UIControlState.Highlighted)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    func deviceRotated() {
+        //updateButton0()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        //updateButton0()
+    }
+    
+    func updateButton0() {
+        button0.contentHorizontalAlignment = .Left
+        let inset = (button0.frame.width/2 - (button0.titleLabel?.intrinsicContentSize().width)!)/2
+        //print("Inset: \(inset)")
+        button0.titleEdgeInsets = UIEdgeInsetsMake(0.0, inset, 0.0, 0.0)
+    }
+    
+    // -= Logic =-
+    var accumulator: Double = 0.0 // Store the calculated value here
+    var userInput = "" // User-entered digits
+    
+    var numberStack: [Double] = [] // Number stack
+    var operatorStack: [String] = [] // Operator stack
+    
+    // Looks for a single character in a string.
+    func hasIndex(stringToSearch string: String, characterToFind charter: Character) -> Bool {
+        for chr in string.characters {
+            if chr == charter {
+                return true
+            }
+        }
+        return false
+    }
+    
+    func operateInput(string: String) {
+        if string == "-" {
+            if userInput.hasPrefix(string) {
+                // Strip off the first character (a dash)
+                userInput = userInput.substringFromIndex(userInput.startIndex.successor())
+            } else {
+                userInput = string + userInput
+            }
+        } else {
+            userInput += string
+        }
+        accumulator = Double((userInput as NSString).doubleValue)
+        updateDisplay()
+    }
+    
+    func updateDisplay() {
+        // If the value is an integer, don't show a decimal point
+        let intAccumulator = Int(accumulator)
+        if accumulator - Double(intAccumulator) == 0 {
+            resultLabel.text = "\(intAccumulator)"
+        } else {
+            resultLabel.text = "\(accumulator)"
+        }
+    }
+    
+    func doMath(newOperation: String) {
+        if userInput != "" && !numberStack.isEmpty {
+            let stackOperation = operatorStack.last
+            if !((stackOperation == "+" || stackOperation == "-") && (newOperation == "*" || newOperation == "/")) {
+                let operation = operations[operatorStack.removeLast()]
+                accumulator = operation!(numberStack.removeLast(), accumulator)
+                doEquals()
+            }
+        }
+        operatorStack.append(newOperation)
+        numberStack.append(accumulator)
+        userInput = ""
+        updateDisplay()
+    }
+    
+    func doEquals() {
+        if userInput == "" {
+            return
+        }
+        if !numberStack.isEmpty {
+            let operation = operations[operatorStack.removeLast()]
+            accumulator = operation!(numberStack.removeLast(), accumulator)
+            if !operatorStack.isEmpty {
+                doEquals()
+            }
+        }
+        updateDisplay()
+        userInput = ""
+    }
 
 }
 
+// Basic mathematic operations
+func addition(a: Double, b: Double) -> Double {
+    let result = a + b
+    return result
+}
+func substraction(a: Double, b: Double) -> Double {
+    let result = a - b
+    return result
+}
+func multiplication(a: Double, b: Double) -> Double {
+    let result = a * b
+    return result
+}
+func division(a: Double, b: Double) -> Double {
+    let result = a / b
+    return result
+}
+
+typealias BinaryOperation = (Double, Double) -> Double
+let operations: [String: BinaryOperation] = [ "+" : addition, "-" : substraction, "*" : multiplication, "/" : division ]
---
+++ StyledLabel.swift
@@ -0,0 +1,141 @@
+//
+//  StyledLabel.swift
+//  Calculator
+//
+//  Created by Admin on 8/8/16.
+//  Copyright © 2016 Yohoho. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class StyledLabel: UILabel
+{
+    @IBInspectable var tracking:CGFloat = 0.8
+    // values between about 0.7 to 1.3.  one means normal.
+    
+    @IBInspectable var stretching:CGFloat = -0.1
+    // values between about -.5 to .5.  zero means normal.
+    
+    override func awakeFromNib()
+    {
+        tweak()
+    }
+    
+    override func prepareForInterfaceBuilder()
+    {
+        tweak()
+    }
+    
+    override func layoutSubviews()
+    {
+        super.layoutSubviews()
+        
+        let tempLabel = StyledLabel()
+        
+        tempLabel.frame = self.frame
+        tempLabel.text = self.text
+        tempLabel.textAlignment = self.textAlignment
+        
+        tempLabel.font = fontToFitHeight()
+        
+        //  fontToFitWidth
+        if (tempLabel.intrinsicContentSize().width>tempLabel.frame.width){
+            repeat {
+                tempLabel.decreaseFontSizeBy(0.5)
+                print("Font decrased by 0.5 to \(tempLabel.font.pointSize)")
+            } while (tempLabel.intrinsicContentSize().width>=tempLabel.frame.width)
+        }
+        
+        self.font = tempLabel.font
+    }
+    
+    private func fontToFitHeight() -> UIFont
+    {
+        /* Apple have failed to include a basic thing needed in handling text: fitting the text to the height. Here's the simplest and fastest way to do that:
+         
+         guess = guess * ( desiredHeight / guessHeight )
+         
+         That's really all there is to it. The rest of the code in this routine is safeguards. Further, the routine iterates a couple of times, which is harmless, to take care of any theoretical bizarre nonlinear sizing issues with strange typefaces. */
+        
+        guard text?.characters.count > 0 else { return font }
+        let desiredHeight:CGFloat = frame.size.height
+        guard desiredHeight>1 else { return font }
+        var guess:CGFloat
+        var guessHeight:CGFloat
+        
+        print("Searching for... ", desiredHeight)
+        
+        guess = font.pointSize
+        if (guess>1&&guess<1000) { guess = 50 }
+        
+        guessHeight = sizeIf(guess)
+        
+        if (guessHeight==desiredHeight)
+        {
+            print("Fluke, exact match within float math limits, up front")
+            return font.fontWithSize(guess)
+        }
+        
+        var iterations:Int = 4
+        
+        /* It is incredibly unlikely you would need more than four iterations, "two" would rarely be needed. You could imagine some very strange glyph handling where the relationship is non-linear (or something weird): That is the only theoretical reason you'd ever need more than one or two iterations. Note that when you watch the output of the iterations, you'll sometimes/often see same or identical values for the result: this is correct and expected in a float iteration. */
+        
+        while(iterations>0)
+        {
+            guess = guess * ( desiredHeight / guessHeight )
+            guessHeight = sizeIf(guess)
+            
+            if (guessHeight==desiredHeight)
+            {
+                print("Unbelievable fluke, exact match within float math limits while iterating")
+                return font.fontWithSize(guess)
+            }
+            
+            iterations -= 1
+        }
+        
+        print("Done. Shame Apple doesn't do this for us!")
+        return font.fontWithSize(guess)
+    }
+    
+    private func sizeIf(pointSizeToTry:CGFloat)->(CGFloat)
+    {
+        let s:CGFloat = text!.sizeWithAttributes(
+            [NSFontAttributeName: font.fontWithSize(pointSizeToTry)] )
+            .height
+        
+        print("Guessing .. ", pointSizeToTry, " .. " , s)
+        return s
+    }
+    
+    private func tweak()
+    {
+        let ats = NSMutableAttributedString(string: self.text!)
+        let rg = NSRange(location: 0, length: self.text!.characters.count)
+        
+        ats.addAttribute(
+            NSKernAttributeName, value:CGFloat(tracking), range:rg )
+        
+        ats.addAttribute(
+            NSExpansionAttributeName, value:CGFloat(stretching), range:rg )
+        
+        self.attributedText = ats
+    }
+    
+    func increaseFontSize() {
+        self.font =  UIFont(name: self.font!.fontName, size: self.font!.pointSize+1)!
+    }
+    
+    func decreaseFontSize() {
+        self.font =  UIFont(name: self.font!.fontName, size: self.font!.pointSize-1)!
+    }
+    
+    func increaseFontSizeBy(size: CGFloat) {
+        self.font =  UIFont(name: self.font!.fontName, size: self.font!.pointSize+size)!
+    }
+    
+    func decreaseFontSizeBy(size: CGFloat) {
+        self.font =  UIFont(name: self.font!.fontName, size: self.font!.pointSize-size)!
+    }
+}
